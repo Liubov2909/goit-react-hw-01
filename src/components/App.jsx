4a49a576e9aa2977000c9b1c8775fe826aa2ca49
Liubov2909 <ostapcuk2909@gmail.com>
2024-02-08 15:36:02 +0200

    part2 for dz 1 react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
-
 import { Profile } from './Profile';
-
+import { FriendList } from './FriendList';
 import userData from "../userData.json";
+import friends from "../friends.json";
 
 export const App = () => {
   return (
     <>
       <Profile info={userData} />
+    
+      <FriendList friends={friends} />
     </>
   );
 };
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,13 @@
+import { FriendListItem } from "./FriendListItem";
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friends-list">
+      {friends.map((friends) => (
+        <li className="friends-item" key={friends.id}> 
+          <FriendListItem friends={friends} />
+        </li>
+      ))}
+    </ul>
+  );
+};
