8756f46e1e6e00c5322d0e3afbc6e4af30cdc9e0
Liubov2909 <ostapcuk2909@gmail.com>
2024-02-08 18:57:23 +0200

    dz 1 react

--- src/components/Friendlist/FriendListItem.jsx
+++ src/components/Friendlist/FriendListItem.jsx
@@ -1,8 +1,8 @@
 export const FriendListItem = ({friends:{avatar, name, isOnline}}) => {
   return(
   <div>
-    <img className="friend-image" src={avatar} alt="Avatar" width="48" />
-    <p className="friend-name-title">{name}</p>
+    <img className="friends-image" src={avatar} alt="Avatar" width="48" />
+    <p className="friends-name-title">{name}</p>
     <p className={isOnline ? 'friends-is-online' : 'friends-offline'}>{isOnline ? 'Online' : 'Offline'}</p>
   </div>
   )
